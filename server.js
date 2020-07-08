da4585abca9ef2927f6a32ee682a0a9de19dc6eb
iy88 <1845767798@qq.com>
2020-07-08 20:38:56 +0800

    update

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const next = require('next')
-
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
+const bodyParse = require('body-parser');
+const compression = require('compression');
+app.use(compression());
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: false }));
+app.disable('x-powered-by');
 app.prepare().then(() => {
   const server = express()
 
