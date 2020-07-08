ee72ba5361a922c89e6d49ef549d770dd3fc2796
iy88 <1845767798@qq.com>
2020-07-08 21:32:18 +0800

    fixed bug

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ class Home extends react.Component {
       try{
         let {data} = await axios.get('/api/isIPv4?ip='+this.state.ip);
         if(data.data.isIPv4){
-          let {data} = this.state.ip === '0.0.0.0' ? await axios.get('https://iy88.site:433/openapi/ipRegion') : await axios.get('https://iy88.site:433/openapi/ipRegion='+this.state.ip);
+          let {data} = this.state.ip === '0.0.0.0' ? await axios.get('https://iy88.site:433/openapi/ipRegion') : await axios.get('https://iy88.site:433/openapi/ipRegion?ip='+this.state.ip);
           this.setState(data.data);
         }else{
           alert('please input an corrent ipv4 address')
